b3c7173d933d6d6d970a53575d462dd3f068f118
Rens-DV-HAN <RJ.deVreede@student.han.nl>
2025-04-14 20:48:20 +0200

    Add today string

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import locale
+from datetime import datetime
+
+locale.setlocale(locale.LC_TIME, "nl_NL.utf8")
+
+###
+
 FILE_PATH = "./logboek-projectweek-1.md"
 
 with open(FILE_PATH) as file:
     text_lines = file.readlines()
+
+###
+
+today = datetime.today()
+today_str = today.strftime("%A %-d %B").capitalize()
+today_str_heading = f"## {today_str}\n"
