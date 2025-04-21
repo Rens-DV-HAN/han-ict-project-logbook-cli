dc12a6296c2edc18426ca51e9e8830f8e61c6c5d
Rens-DV-HAN <RJ.deVreede@student.han.nl>
2025-04-21 20:20:32 +0200

    Add value input with defaults

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import locale
+import math
+import os.path
 from datetime import datetime
+
+import questionary
 
 locale.setlocale(locale.LC_TIME, "nl_NL.utf8")
 
@@ -12,8 +16,8 @@ with open(FILE_PATH) as file:
 
 ###
 
-today = datetime.today()
-today_str = today.strftime("%A %-d %B").capitalize()
+now = datetime.now()
+today_str = now.strftime("%A %-d %B").capitalize()
 today_str_heading = f"## {today_str}\n"
 
 ###
@@ -42,17 +46,34 @@ if new_row_index is None:
 
 ###
 
-columns = [
-    "Spent hours",
-    "Description",
-    "Those involved",
-    "Result",
-    "Emoji",
-    "Link",
-]
+last_file_modification_timestamp = os.path.getmtime(FILE_PATH)
+last_file_modification_datetime = datetime.fromtimestamp(
+    last_file_modification_timestamp
+)
 
-new_row_values = [input(f"{column}: ") for column in columns]
-new_row = "| " + " | ".join(new_row_values) + " |\n"
+hours_since_last_file_modification = (
+    now - last_file_modification_datetime
+).total_seconds() / 3600
+hours_since_last_file_modification_rounded = (
+    math.ceil(hours_since_last_file_modification * 2) / 2
+)
+hours_since_last_file_modification_rounded_str = str(
+    hours_since_last_file_modification_rounded
+).replace(".", ",")
+
+new_row_data = {
+    "Tijd": hours_since_last_file_modification_rounded_str,
+    "Beschrijving": "",
+    "Met wie": "N.v.t.",
+    "Resultaat": "",
+    "Emoji": "",
+    "Link": "N.v.t.",
+}
+
+for column, value in new_row_data.items():
+    new_row_data[column] = questionary.text(f"{column}: ", default=value).ask()
+
+new_row = "| " + " | ".join(new_row_data.values()) + " |\n"
 
 text_lines.insert(new_row_index, new_row)
 
